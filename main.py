d6d229ce14833161fdce8e06d66e2dc1b62f1c0a
Aurélien Petit <david.jacques@celeste.fr>
2025-12-04 14:33:50 +0100

    fix: PromptTemplate

--- main.py
+++ main.py
@@ -22,21 +22,19 @@ def main():
 
     summary_template = """
     given the information:
-    \"\"\"{Info}\"\"\"
+    \"\"\"{information}\"\"\"
     about a person, I want you to create:
     1. A short summary
     2. two interesting facts about him/her.
     """
 
-    summary_prompt_template = PromptTemplate(
-        input_variables=["Info"], template=summary_template
-    )
+    summary_prompt_template = PromptTemplate(template=summary_template)
 
     llm = ChatOllama(temperature=0, model="qwen3-vl:2b")
 
     chain = summary_prompt_template | llm
 
-    response = chain.invoke(input={"Info": information})
+    response = chain.invoke(input={"information": information})
     print(response.content)
 
 if __name__ == "__main__":
