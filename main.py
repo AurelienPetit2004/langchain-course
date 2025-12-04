a3685a59003c9849864e87ae707161f6e07076ff
Aurélien Petit <david.jacques@celeste.fr>
2025-12-04 12:00:36 +0100

    feat: langsmith linked

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 def main():
     print("Hello from langchain-course!")
+
     information = """
     Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
 
@@ -17,18 +21,22 @@ def main():
     """
 
     summary_template = """
-    given the information {information} about a person, I want you to create:
+    given the information:
+    \"\"\"{Info}\"\"\"
+    about a person, I want you to create:
     1. A short summary
     2. two interesting facts about him/her.
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["Info"], template=summary_template
     )
 
-    llm = ChatOllama(temperature=0, model="llama3.2:3b")
+    llm = ChatOllama(temperature=0, model="qwen3-vl:2b")
+
     chain = summary_prompt_template | llm
-    response = chain.invoke(input={"information": information})
+
+    response = chain.invoke(input={"Info": information})
     print(response.content)
 
 if __name__ == "__main__":
